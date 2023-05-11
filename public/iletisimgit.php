bb011248b8ef40344f78f78f3ee0d92c36e5c0cd
iremkabil <114577676+iremkabil@users.noreply.github.com>
2023-05-11 12:32:53 +0300

    php eklendi

---
+++ public/iletisimgit.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Prompt&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="css/main.css">
+    <title>Document</title>
+</head>
+<body>
+        
+    <!-- header -->
+    <header id="header__inner">
+        <div class="main">
+            <div class="main-logo">
+                <a href="index.html" >İrem Kabil</a>
+            </div>
+            <nav class="main-nav">
+                <ul class="main-nav__items">
+                    <li><a href="ozgecmis.html">Özgeçmiş</a></li>
+                    <li><a href="sehrim.html">Şehrim</a></li>
+                    <li><a href="takim.html">Takımımız</a></li>
+                    <li><a href="ilgi-alanlarim.html">İlgi Alanlarım</a></li>
+                    <li><a href="iletisim.html" class="active">İletişim Sayfası</a></li>
+
+                </ul>
+            </nav>
+            <div class="main-login">
+                <button class="btn btn-dark main-nav__login"><a href="login.html">Login</a></button>
+            </div>
+        </div>
+    </header>
+
+    <section id="abilities3" class="text-white mx-5 mb-5">
+        <div class="bg-dark container border border-secondary rounded shadow-lg">
+          <div class="row my-3 ">
+            <div class="col-lg-12 my-4">
+              <h1 class="display-3 text-white"  >İletisim Bilgileri</h1>
+            </div>
+          </div>
+          <div class="row my-1 ">
+            <div class="col-lg-12 my-1">
+            <table   class="table table-striped table-dark my-table" >
+            	<tr>
+            	   <td >Ad</td>
+            	   <td ><?php echo $_POST["ad"]?></td>
+            	</tr>
+              <tr>
+                <td>Mail</td>
+                <td ><?php echo $_POST["email"]?></td>
+            	</tr>
+              <tr>
+                <td>Sifre</td>
+                <td ><?php echo $_POST["sifre"]?></td>
+              </tr>
+              <tr>
+                <td>Hobiler</td>
+                <td >
+                  <?php if(isset($_POST['sports']))
+                  {
+                    $check=implode("-",$_POST['sports']);
+                    echo($check);
+                  }
+                  ?>
+              </td>
+              </tr>
+              <tr>
+                <td>En Sevilen Programlama Dili</td>
+                <td ><?php echo $_POST["language"]?></td>
+              </tr>
+              <tr>
+                <td>Adres</td>
+                <td ><?php echo $_POST["addres"]?></td>
+              </tr>
+              <tr>
+                <td>Yaşadığınız Şehir</td>
+                <td ><?php echo $_POST["sehirler"]?></td>
+              </tr>
+                </table>
+              </div>
+            </div>
+        </div>
+      </section>
+
+
+    <!-- section b -->
+    <section id="iletisim-b" class="bg-pri py-4">
+        <div class="container">
+            <div class="contact-info">
+                <div>
+                    <i class="fas fa-envelope fa-2x"></i>
+                    <h3>Email</h3>
+                    <p>iremkabil54@gmail.com</p>
+                </div>
+                <div>
+                    <i class="fas fa-phone fa-2x"></i>
+                    <h3>Phone</h3>
+                    <p>(555) 555 55 55</p>
+                </div>
+                <div>
+                    <i class="fas fa-address-card fa-2x"></i>
+                    <h3>Address</h3>
+                    <p>Sakarya/Türkiye</p>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <footer id="main-footer">
+        <div class="footer-content container">
+            <p>Copyright &copy; 2023. All Rights Reserved.</p>
+            <div class="social">
+                <a href="https://github.com/iremkabil"><i class="fab fa-brands fa-github"></i></a>
+                
+                <a href="mailto:iremkabil54@gmail.com"><i class="fab fa-thin fa-at"></i></a>
+
+                <a href="https://www.linkedin.com/in/irem-kabil-6387b926b/"><i class="fab fa-brands fa-linkedin"></i></a>
+
+            </div>
+        </div>
+    </footer>
+
+</body>
+</html> 
---
+++ public/logingit.php
@@ -0,0 +1,141 @@
+<?php 
+  
+  $myemail="CC@hotmail.com";
+  $mysifre="123";
+  $email = $_POST['email'];
+  $sifre = $_POST['sifre'];
+  $namen=explode("@",$email);
+  $signal=false;
+  if($email==$myemail && $mysifre==$sifre)
+  {
+    $signal=true;
+    
+  }else {
+    $signal=false;
+    header("refresh:5;url=login.html");
+
+    
+    
+    
+    //header("Location:login.html");
+  }
+?>
+<html lang="en">
+  <head>
+
+  <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Prompt&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!--  CSS -->
+    <link rel="stylesheet" href="css/main.css">
+
+  </head>
+  <body>
+  <header id="header__inner">
+        <div class="main">
+            <div class="main-logo">
+                <a href="index.html" >İrem Kabil</a>
+            </div>
+            <nav class="main-nav">
+                <ul class="main-nav__items">
+                    <li><a href="ozgecmis.html">Özgeçmiş</a></li>
+                    <li><a href="sehrim.html">Şehrim</a></li>
+                    <li><a href="takim.html">Takımımız</a></li>
+                    <li><a href="ilgi-alanlarim.html">İlgi Alanlarım</a></li>
+                    <li><a href="iletisim.html">İletişim Sayfası</a></li>
+                </ul>
+            </nav>
+            <div class="main-login">
+                <button class="btn btn-dark main-nav__login"><a href="login.html" class=" active">Login</a></button>
+            </div>
+        </div>
+    </header>
+
+    <section id="login" class="text-center py-3">
+        <div class="container">
+            <h2 class="section-title">Login</h2>
+            <div class="border-bottom"></div>
+            <p class="lead">Lütfen giriş yapınız...</p>
+            <!-- <form action="">
+                <div class="form-login">
+                    <div class="my-4">
+                        Email: CC@hotmail.com Şifre: 123
+                    </div>
+                    <input type="email" class="email input" name="email" id="email" placeholder="Email">
+
+                    <input type="password" class="password input" name="password" id="password" placeholder="password">
+
+                </div>
+            </form> -->
+
+            <form action="logingit.php" name="myForm" class="needs-validation" onsubmit = "return validate();"  method="POST">
+              <div class="form-row my-3">Email: CC@hotmail.com   Şifre: 123
+              </div>
+              <div class="form-row my-3">
+                <div class="col-lg-4 my-3">
+                  <label for="email">Email:</label>
+                  <input type="text" class="form-control" name="email" id="email" onchange="emailV();">
+                  <div class="valid-feedback">
+                    Kriterlere uyuyor
+                  </div>
+                  <div class="invalid-feedback">
+                    Alan boş yada Girdiğiniz bilgiler E mail formatında DEĞİL!!
+                  </div>
+                </div>
+              </div>
+              <div class="form-row my-3">
+                <div class="col-lg-4 mb-3">
+                  <label for="password">Şifre:</label>
+                  <input type="password"class="form-control" name="sifre" onchange="sifreV();">
+                  <div class="invalid-feedback">
+                    Alan boş yada Şifre rakamdan başka karakter içeriyor
+                  </div>
+                  <div class="valid-feedback">
+                    Kriterlere Uyuyor
+                  </div>
+                </div>
+              </div>
+              <div class="form-row">
+                <div class="col-lg-12 mb-3">
+                  <input type="submit"  class="btn btn-primary" value="Gonder">
+                </div>
+              </div>        
+            </form>
+
+            <!-- <button type="submit" class="btn-dark">Submit</button> -->
+
+        </div>
+    </section>
+    <section id="abilities3" class="text-white mx-5 mb-5">
+      <div class="bg-dark container border border-secondary rounded shadow-lg">
+        <div class="row my-3 ">
+          <div class="col-lg-12 my-4">
+          <!-- align="center" css'te ver -->
+            <h1 class="display-4 text-white"  >
+                <?php
+                  if($signal==true)
+                  {
+                    echo $namen[0]."  Hoşgeldin" ;
+                  }
+                  else
+                  {
+                    echo "Kullanıcı epostası yada şifre hatalı ";
+                    echo "5 saniye içinde geri yönlendiriliyorsunuz";
+                  }
+                ?>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </body>
+<html>
